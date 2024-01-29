7504ecfadeaa9c8be856258df3e0bb6951ed1cb4
김초향 <chkim9910@naver.com>
2024-01-29 15:43:47 +0900

    add <BrowserRouter></BrowserRouter>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./views";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    // router 버전이 6이상이기 때문에 <BrowserRouter></BrowserRouter>로 감싸줘야 함
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
